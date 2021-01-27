966b33a5667d22fd7bac3041e8d2c43fe83e74e9
dyzcs <deng.kinger@gmail.com>
2021-01-27 21:45:20 +0800

    Update SourceTest.scala

--- src/main/scala/com/dyzcs/SourceTest.scala
+++ src/main/scala/com/dyzcs/SourceTest.scala
@@ -1,6 +1,9 @@
 package com.dyzcs
 
+import org.apache.flink.streaming.api.functions.source.SourceFunction
 import org.apache.flink.streaming.api.scala._
+
+import scala.util.Random
 
 /**
  * Created by Administrator on 2021/1/27.
@@ -26,9 +29,43 @@ object SourceTest {
         stream1.print("flink source from collection")
 
         // 2.从文件中读取数据
-        val stream2 = env.readTextFile("sensor.txt")
+        val stream2 = env.readTextFile("src/main/resources/sensor.txt")
         stream2.print("flink source from file")
 
+        // 4.自定义source
+        val stream4 = env.addSource(new MySensorSource)
+        stream4.print("flink custom source")
+
         env.execute()
+    }
+}
+
+class MySensorSource extends SourceFunction[SensorReading] {
+    // 定义一个标识位，用来表示数据源是否正常运行发出数据
+    var running = true
+
+    override def run(ctx: SourceFunction.SourceContext[SensorReading]): Unit = {
+        // 定义一个随机数发生器
+        val rand = new Random()
+        // 随机生成一组(10个)传感器的初始温度: (id, temp)
+        var curTemp = 1.to(10).map(i => ("sensor_" + i, 60 + rand.nextGaussian() * 20))
+        // 定义无限循环，不停的产生数据，除非被cancel
+        while (running) {
+            // 在上次数据基础上微调，更新温度值
+            curTemp = curTemp.map(
+                data => (data._1, data._2 + rand.nextGaussian())
+            )
+            // 获取当前时间戳，加入到数据中，调用ctx.collect发出数据
+            val curTime = System.currentTimeMillis()
+            curTemp.foreach(
+                data => ctx.collect(SensorReading(data._1, curTime, data._2))
+            )
+            // 间隔500ms
+            Thread.sleep(500)
+        }
+    }
+
+    override def cancel(): Unit = {
+        running = false
     }
 }
