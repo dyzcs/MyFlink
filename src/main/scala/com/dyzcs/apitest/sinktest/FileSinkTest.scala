369ff096db80eb41707b8a37e81a1b1c45f912b1
dyzcs <deng.kinger@gmail.com>
2021-01-28 11:16:35 +0800

    writeAsCsv

--- src/main/scala/com/dyzcs/apitest/sinktest/FileSinkTest.scala
+++ src/main/scala/com/dyzcs/apitest/sinktest/FileSinkTest.scala
@@ -1,5 +1,6 @@
 package com.dyzcs.apitest.sinktest
 
+import com.dyzcs.apitest.SensorReading
 import org.apache.flink.streaming.api.scala._
 
 /**
@@ -11,8 +12,18 @@ object FileSinkTest {
         env.setParallelism(1)
 
         // 读取数据
-        val inputData = env.readTextFile("src/main/resources/sensor.txt")
+        val inputStream = env.readTextFile("src/main/resources/sensor.txt")
 
         // 先转换成样例类类型
+        val dataStream = inputStream.map(data => {
+            val arr = data.split(",")
+            SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+        })
+        // 控制台输出
+//        dataStream.print()
+
+        dataStream.writeAsCsv("src/main/resources/out.csv")
+
+        env.execute("file sink to file")
     }
 }
