d3c38b0be745629e76ebae9613add9383abde69a
dyzcs <deng.kinger@gmail.com>
2021-01-28 22:04:50 +0800

    Update TransformTest.scala

--- src/main/scala/com/dyzcs/TransformTest.scala
+++ src/main/scala/com/dyzcs/TransformTest.scala
@@ -1,6 +1,7 @@
 package com.dyzcs
 
 import com.dyzcs.apitest.SensorReading
+import org.apache.flink.api.common.functions.FilterFunction
 import org.apache.flink.streaming.api.scala._
 
 /**
@@ -16,5 +17,28 @@ object TransformTest {
             val arr = data.split(",")
             SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
         })
+
+        // 继承FilterFunction完成过滤
+        val filterStream = dataStream.filter(new MyFilter)
+        filterStream.print("filter")
+
+        // 分组聚合，输出每个传感器当前最小值
+        // 根据id分组
+        val aggStream = dataStream.keyBy(_.id)
+                .minBy("temperature")
+        aggStream.print("agg")
+
+        val resultStream1 = dataStream.keyBy(_.id)
+                .reduce((curState, newData) => {
+                    SensorReading(curState.id, newData.timestamp, curState.temperature.min(newData.temperature))
+                })
+        resultStream1.print("result")
+
+        env.execute("flink transform test")
     }
+}
+
+class MyFilter extends FilterFunction[SensorReading] {
+    override def filter(value: SensorReading): Boolean =
+        value.id.startsWith("sensor_1")
 }
