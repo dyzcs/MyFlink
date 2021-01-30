1c0b4b43d8b6f47eed3e14dd9d416860a35111d2
dyzcs <deng.kinger@gmail.com>
2021-01-30 11:20:47 +0800

    窗口函数

--- src/main/scala/com/dyzcs/apitest/WindowTest.scala
+++ src/main/scala/com/dyzcs/apitest/WindowTest.scala
@@ -1,5 +1,6 @@
 package com.dyzcs.apitest
 
+import org.apache.flink.api.common.functions.ReduceFunction
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.{EventTimeSessionWindows, SlidingEventTimeWindows, TumblingEventTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
@@ -20,9 +21,22 @@ object WindowTest {
 
         // 每15秒统计一次，窗口内各传感器温度的最小值，以及最新的时间戳
         val resultStream = dataStream.keyBy(_.id)
-//                .window(TumblingEventTimeWindows.of(Time.seconds(15)))  // 滚动时间窗口
-//                .window(SlidingEventTimeWindows.of(Time.seconds(15), Time.seconds(5)))  // 滑动时间窗口
-//                .window(EventTimeSessionWindows.withGap(Time.seconds(10)))  // 会话窗口
-//                .countWindow(10)    // 滚动计数窗口
+                // .timeWindow(Time.seconds(15)) // 滚动时间窗口简写[已弃用]
+                // .window(SlidingEventTimeWindows.of(Time.seconds(15), Time.seconds(5)))  // 滑动时间窗口
+                // .window(EventTimeSessionWindows.withGap(Time.seconds(10)))  // 会话窗口
+                // .countWindow(10)    // 滚动计数窗口
+                .window(TumblingEventTimeWindows.of(Time.seconds(15))) // 滚动时间窗口
+//                .reduce((curRes, newRes) => SensorReading(curRes.id, curRes.timestamp.max(newRes.timestamp), curRes.temperature.min(newRes.temperature)))
+                .reduce(new MyReducer)
+
+        resultStream.print("result")
+
+        env.execute("window test")
+    }
+}
+
+class MyReducer extends ReduceFunction[SensorReading] {
+    override def reduce(t1: SensorReading, t2: SensorReading): SensorReading = {
+        SensorReading(t1.id, t1.timestamp.max(t2.timestamp), t1.temperature.min(t2.temperature))
     }
 }
